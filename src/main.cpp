eeced5f098ec3ffcb89ba70a4bec401cd98d134f
dogsc729 <b07611033@ntu.edu.tw>
2020-11-20 11:51:11 +0800

    table construction complete

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@
 #include "../lib/tm_usage.h"
 
 using namespace std;
-void MAXIMUM_PLANAR_SUBSET(int, int);
+void MAXIMUM_PLANAR_SUBSET(map<int, int>, int);
 void help_message()
 {
     cout << "usage: ./mps <input file name> <output file name>" << endl;
@@ -40,35 +40,57 @@ int main(int argc, char *argv[])
     fin >> size;
     cout << size << endl;
     int i, j;
-    map<int, int> data;
+    map<int, int> chord;
     while (fin >> i >> j)
     {
-        data[i] = j;
-        data[j] = i;
-        cout << data[i] << endl;
-        cout << data[j] << endl;
+        chord[i] = j;
+        chord[j] = i;
     }
+    MAXIMUM_PLANAR_SUBSET(chord, size / 2);
     return 0;
 }
 
-void MAXIMUM_PLANAR_SUBSET(int c, int n)
+void MAXIMUM_PLANAR_SUBSET(map<int, int> chord, int n)
 {
-    int **M = new int*[2*n]
-    for(int i=0;i<(2*n);i++)
-    M[i] = new int[2*n]
-    for (int i = 0; i < (2*n - 1); i++)
+    int **M = new int *[2 * n];
+    for (int i = 0; i < (2 * n); i++)
+        M[i] = new int[2 * n];
+
+    for (int i = 0; i < 2 * n; i++)
     {
         M[i][i] = 0;
     }
-    for (int l = 0; l < (2*n - 1); l++)
+    for (int l = 1; l < 2 * n; l++)
     {
-        for (int i = 0; i < (2*n - l); i++)
+        for (int i = 0; i < 2 * n - l; i++)
         {
             int j = i + l;
-            if (chord kj is in c && !(k >= i && k <= j))
+            if (!(chord[j] >= i && chord[j] <= j))
                 M[i][j] = M[i][j - 1];
-            else if ()
+            else if (chord[j] == i)
+                M[i][j] = M[i + 1][j - 1] + 1;
+            else
+            {
+                if (M[i][j - 1] >= M[i][chord[j] - 1] + 1 + M[chord[j] + 1][j - 1])
+                {
+                    M[i][j] = M[i][j - 1];
+                }
+                else
+                {
+                    M[i][j] = M[i][chord[j] - 1] + 1 + M[chord[j] + 1][j - 1];
+                }
+            }
         }
+    }
+    for (int i = 0; i < 2 * n; i++)
+    {
+        cout << chord[i] << endl;
+    }
+    for (int i = 0; i < 2 * n; i++)
+    {
+        cout << endl;
+        for (int j = 0; j < 2 * n; j++)
+            cout << M[i][j];
     }
     return;
 }

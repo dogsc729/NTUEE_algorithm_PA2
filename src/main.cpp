fc8f5f91382ac1920d8b6159253de98ee76878ea
dogsc729 <b07611033@ntu.edu.tw>
2020-11-21 11:47:33 +0800

    overall complete

--- src/main.cpp
+++ src/main.cpp
@@ -14,9 +14,10 @@
 #include <new>
 #include <ctime>
 #include "../lib/tm_usage.h"
+#include <map>
 
 using namespace std;
-void MAXIMUM_PLANAR_SUBSET(map<int, int>, int);
+int **MAXIMUM_PLANAR_SUBSET(std::map<int, int>, int);
 void help_message()
 {
     cout << "usage: ./mps <input file name> <output file name>" << endl;
@@ -38,7 +39,6 @@ int main(int argc, char *argv[])
     fout.open(argv[2], ios::out);
     int size;
     fin >> size;
-    cout << size << endl;
     int i, j;
     map<int, int> chord;
     while (fin >> i >> j)
@@ -46,51 +46,54 @@ int main(int argc, char *argv[])
         chord[i] = j;
         chord[j] = i;
     }
-    MAXIMUM_PLANAR_SUBSET(chord, size / 2);
+    ////////////get the table///////////////////////////
+    int **table = MAXIMUM_PLANAR_SUBSET(chord, size / 2);
+    /////////////print out the size/////////////////////
+    fout << table[0][size - 1] << endl;
+    /////////////print out the chord////////////////////
+    for (int i = 0; i < size - 1; i++)
+    {
+        if (table[i][size - 1] - table[i + 1][size - 1] > 0)
+            fout << i << " " << chord[i] << endl;
+    }
+    fin.close();
+    fout.close();
     return 0;
 }
-
-void MAXIMUM_PLANAR_SUBSET(map<int, int> chord, int n)
+///////////////construct the table///////////////////////////
+int **MAXIMUM_PLANAR_SUBSET(map<int, int> chord, int n)
 {
-    int **M = new int *[2 * n];
-    for (int i = 0; i < (2 * n); i++)
-        M[i] = new int[2 * n];
+    int size = 2 * n;
+    int **M = new int *[size];
+    for (int i = 0; i < (size); i++)
+        M[i] = new int[size];
 
-    for (int i = 0; i < 2 * n; i++)
+    for (int i = 0; i < size; i++)
     {
         M[i][i] = 0;
     }
-    for (int l = 1; l < 2 * n; l++)
+    for (int l = 1; l < size; l++)
     {
-        for (int i = 0; i < 2 * n - l; i++)
+        for (int i = 0; i < size - l; i++)
         {
             int j = i + l;
-            if (!(chord[j] >= i && chord[j] <= j))
+            int k = chord[j];
+            if (!(k >= i && k <= j))
                 M[i][j] = M[i][j - 1];
-            else if (chord[j] == i)
+            else if (k == i)
                 M[i][j] = M[i + 1][j - 1] + 1;
             else
             {
-                if (M[i][j - 1] >= M[i][chord[j] - 1] + 1 + M[chord[j] + 1][j - 1])
+                if (M[i][j - 1] >= M[i][k - 1] + 1 + M[k + 1][j - 1])
                 {
                     M[i][j] = M[i][j - 1];
                 }
                 else
                 {
-                    M[i][j] = M[i][chord[j] - 1] + 1 + M[chord[j] + 1][j - 1];
+                    M[i][j] = M[i][k - 1] + 1 + M[k + 1][j - 1];
                 }
             }
         }
     }
-    for (int i = 0; i < 2 * n; i++)
-    {
-        cout << chord[i] << endl;
-    }
-    for (int i = 0; i < 2 * n; i++)
-    {
-        cout << endl;
-        for (int j = 0; j < 2 * n; j++)
-            cout << M[i][j];
-    }
-    return;
+    return M;
 }
